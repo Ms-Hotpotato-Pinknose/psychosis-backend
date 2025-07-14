a658bf7df8130d6e608b267559643eeb4bd53744
Ms-Hotpotato-Pinknose <lewis.melissa.andrea@gmail.com>
2025-07-14 17:49:01 -0400

    Render changes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -13,4 +14,5 @@ def assess():
     return jsonify({"score": score, "result": result})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
